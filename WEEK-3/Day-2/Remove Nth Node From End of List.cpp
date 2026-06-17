a6771f3e4987995208ebb6ceebcfefa4112332fd
Vivek-2908 <sid.k7985@gmail.com>
2026-06-17 18:21:17 +0530

    Implement function to remove Nth node from end

--- WEEK-3/Day-2/Remove Nth Node From End of List.cpp
+++ WEEK-3/Day-2/Remove Nth Node From End of List.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode A(0);
+        A.next = head;
 
+        ListNode* fast =&A;
+        ListNode* slow = &A;
+
+        for (int i = 0; i <= n; i++) {
+            fast = fast->next;
+        }
+
+        while (fast != NULL) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        slow->next = slow->next->next;
+
+        return A.next;
+    }
+};
