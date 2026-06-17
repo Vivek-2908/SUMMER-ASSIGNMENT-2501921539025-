12f7458b7624da5100f72b3139b580385acc1d64
Vivek-2908 <sid.k7985@gmail.com>
2026-06-17 18:40:47 +0530

    Implement MinStack with push, pop, top, and getMin

--- WEEK-3/Day-3/Min Stack.cpp
+++ WEEK-3/Day-3/Min Stack.cpp
@@ -1,1 +1,31 @@
+class MinStack {
+public:
+    stack<int> A;
+    stack<int> B;
 
+    MinStack() {
+    }
+
+    void push(int val) {
+        A.push(val);
+
+        if (B.empty() || val <= B.top()) {
+            B.push(val);
+        }
+    }
+
+    void pop() {
+        if (A.top() == B.top()) {
+            B.pop();
+        }
+        A.pop();
+    }
+
+    int top() {
+        return A.top();
+    }
+
+    int getMin() {
+        return B.top();
+    }
+};
