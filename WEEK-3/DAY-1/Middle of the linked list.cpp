fe3489dde028daaaad92fc9e1a725e8207365bc3
Vivek-2908 <sid.k7985@gmail.com>
2026-06-17 18:00:08 +0530

    Implement function to find the middle of a linked list

--- WEEK-3/DAY-1/Middle of the linked list.cpp
+++ WEEK-3/DAY-1/Middle of the linked list.cpp
@@ -1,1 +1,14 @@
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+         ListNode* A = head;
+        ListNode* B = head;
 
+        while (B != NULL && B->next != NULL) {
+            A = A->next;
+            B= B->next->next;
+        }
+
+        return A;
+    }
+};
