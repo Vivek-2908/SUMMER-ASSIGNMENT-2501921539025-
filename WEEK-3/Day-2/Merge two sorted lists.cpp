602edc6997d1ee42f3659cc30b7502b72cb456a3
Vivek-2908 <sid.k7985@gmail.com>
2026-06-17 18:11:01 +0530

    Implement mergeTwoLists function to merge sorted lists

--- WEEK-3/Day-2/Merge two sorted lists.cpp
+++ WEEK-3/Day-2/Merge two sorted lists.cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode intel;
+        ListNode* core = &intel;
 
+        while (list1 != NULL && list2 != NULL) {
+            if (list1->val <= list2->val) {
+                core->next = list1;
+                list1 = list1->next;
+            } else {
+                core->next = list2;
+                list2 = list2->next;
+            }
+            core = core->next;
+        }
+
+        if (list1 != NULL) {
+            core->next = list1;
+        } else {
+            core->next = list2;
+        }
+
+        return intel.next;
+    }
+};
