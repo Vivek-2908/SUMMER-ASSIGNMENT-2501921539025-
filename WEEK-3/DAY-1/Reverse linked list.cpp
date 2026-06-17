7813d6e19a43a1227eba10ab0e49f893682faa67
Vivek-2908 <sid.k7985@gmail.com>
2026-06-17 17:47:00 +0530

    reverse linked list function

--- WEEK-3/DAY-1/Reverse linked list.cpp
+++ WEEK-3/DAY-1/Reverse linked list.cpp
@@ -1,1 +1,17 @@
+class Solution {
+public:
+ ListNode* reverseList(ListNode* head) {
+        ListNode* back = NULL;
+        ListNode* present = head;
+
+        while (present != NULL) {
+            ListNode* nextNode = present->next;
+            present->next = back;
+            back = present;
+            present = nextNode;
+        }
+
+        return back;
+    }
+};
 
