e17a7baba3f63f2b07c5dc0cec131b546f6a52b5
VIVEK KR_CODES <sid.k7985@gmail.com>
2026-06-18 12:52:11 +0530

    Implement Reverse Polish Notation evaluation

--- WEEK-3/Day-4/Evaluate Reverse Polish Notation.cpp
+++ WEEK-3/Day-4/Evaluate Reverse Polish Notation.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> s;
 
+        for (int i = 0; i < tokens.size(); i++) {
+            string t = tokens[i];
+
+            if (t == "+" || t == "-" || t == "*" || t == "/") {
+                int num2 = s.top();
+                s.pop();
+
+                int num1 = s.top();
+                s.pop();
+
+                if (t == "+") s.push(num1 + num2);
+                if (t == "-") s.push(num1 - num2);
+                if (t == "*") s.push(num1 * num2);
+                if (t == "/") s.push(num1 / num2);
+            }
+            else {
+                s.push(stoi(t));
+            }
+        }
+
+        return s.top();
+    }
+};
