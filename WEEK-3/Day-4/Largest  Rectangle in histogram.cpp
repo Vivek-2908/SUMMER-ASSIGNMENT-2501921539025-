5371ddc2ecf009b63642df7c11afd7eeb594d616
VIVEK KR_CODES <sid.k7985@gmail.com>
2026-06-18 13:04:22 +0530

    Implement largest rectangle area in histogram

--- WEEK-3/Day-4/Largest  Rectangle in histogram.cpp
+++ WEEK-3/Day-4/Largest  Rectangle in histogram.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        int maxA = 0;
 
+        for (int i = 0; i < n; i++) {
+            int minH = heights[i];
+
+            for (int j = i; j < n; j++) {
+                minH = min(minH, heights[j]);
+                int area = minH * (j - i + 1);
+                maxA = max(maxA, area);
+            }
+        }
+
+        return maxA;
+    }
+};
