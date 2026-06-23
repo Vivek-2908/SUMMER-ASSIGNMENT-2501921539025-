781557e82a1782957933de93d028d45c7b2baebf
VIVEK KR_CODES <sid.k7985@gmail.com>
2026-06-23 13:07:04 +0530

    Implement search function for Binary Search Tree

--- WEEK 4 TREES/DAY -3 Binary Search tree/Search in a Binary Search Tree .cpp
+++ WEEK 4 TREES/DAY -3 Binary Search tree/Search in a Binary Search Tree .cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int A) {
+        while (root != NULL) {
+            if (root->val ==A)
+                return root;
+            else if (A < root->val)
+                root = root->left;
+            else
+                root = root->right;
+        }
+        return NULL;
+    }
+};
