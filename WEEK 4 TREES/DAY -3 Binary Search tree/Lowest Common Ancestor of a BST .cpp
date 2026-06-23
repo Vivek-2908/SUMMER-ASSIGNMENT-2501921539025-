d2beed5c90df499f26493897b502875220faf45f
VIVEK KR_CODES <sid.k7985@gmail.com>
2026-06-23 13:33:24 +0530

    Implement lowestCommonAncestor method in Solution class

--- WEEK 4 TREES/DAY -3 Binary Search tree/Lowest Common Ancestor of a BST .cpp
+++ WEEK 4 TREES/DAY -3 Binary Search tree/Lowest Common Ancestor of a BST .cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* A, TreeNode* p, TreeNode* q) {
+        while (A) {
+            if (p->val < A->val && q->val < A->val)
+                A = A->left;
+            else if (p->val > A->val && q->val > A->val)
+                A =A->right;
+            else
+                return A;
+        }
+        return NULL;
+    }
+};
