cba614dbabe3f40a214d6000f5746f7f8c35e630
VIVEK KR_CODES <sid.k7985@gmail.com>
2026-06-23 13:15:26 +0530

    Update Validate Binary Search Tree.cpp

--- WEEK 4 TREES/DAY -3 Binary Search tree/Validate Binary Search Tree.cpp
+++ WEEK 4 TREES/DAY -3 Binary Search tree/Validate Binary Search Tree.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    vector<int> v;
 
+    void inorder(TreeNode* root) {
+        if (root == NULL) return;
+
+        inorder(root->left);
+        v.push_back(root->val);
+        inorder(root->right);
+    }
+
+    bool isValidBST(TreeNode* root) {
+        inorder(root);
+
+        for (int i = 1; i < v.size(); i++) {
+            if (v[i] <= v[i - 1])
+                return false;
+        }
+
+        return true;
+    }
+};
