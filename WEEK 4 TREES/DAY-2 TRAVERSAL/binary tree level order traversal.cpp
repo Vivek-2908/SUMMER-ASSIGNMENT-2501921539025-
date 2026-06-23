ccb3afa31cc18c518b66f8e035ddf9440b17b60c
VIVEK KR_CODES <sid.k7985@gmail.com>
2026-06-23 12:08:06 +0530

    Implement level order traversal for binary tree

--- WEEK 4 TREES/DAY-2 TRAVERSAL/binary tree level order traversal.cpp
+++ WEEK 4 TREES/DAY-2 TRAVERSAL/binary tree level order traversal.cpp
@@ -1,1 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* A) {
+        vector<vector<int>> ans;
+        if (!A) return ans;
 
+        queue<TreeNode*> q;
+        q.push(A);
+
+        while (!q.empty()) {
+            int n = q.size();
+            vector<int> level;
+
+            while (n--) {
+                TreeNode* cur = q.front();
+                q.pop();
+
+                level.push_back(cur->val);
+
+                if (cur->left) q.push(cur->left);
+                if (cur->right) q.push(cur->right);
+            }
+
+            ans.push_back(level);
+        }
+
+        return ans;
+    }
+};
