ba34dd93db20488d85ab042c70a7db7bee20bd72
VIVEK KR_CODES <sid.k7985@gmail.com>
2026-06-26 11:49:59 +0530

    Implement maximum path sum for binary tree

--- WEEK 4 TREES/ DAY-4 Path Based Problems/Binary Tree Maximum Path Sum .cpp
+++ WEEK 4 TREES/ DAY-4 Path Based Problems/Binary Tree Maximum Path Sum .cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
+    int maxi = INT_MIN;
 
+    int solve(TreeNode* root) {
+        if (root == NULL)
+            return 0;
+
+        int A = solve(root->left);
+        int B = solve(root->right);
+
+        if (A < 0)
+            A= 0;
+        if (B< 0)
+            B = 0;
+
+        maxi = max(maxi, A + B + root->val);
+
+        return root->val + max(A, B);
+    }
+
+    int maxPathSum(TreeNode* root) {
+        solve(root);
+        return maxi;
+    }
+};
