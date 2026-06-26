8d69881106c211e10eaa726b42fa153638eb395d
VIVEK KR_CODES <sid.k7985@gmail.com>
2026-06-26 12:03:54 +0530

    Implement binary tree construction from traversals

--- WEEK 4 TREES/DAY-5  Advanced Trees/Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ WEEK 4 TREES/DAY-5  Advanced Trees/Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    int i = 0;
 
+    TreeNode* solve(vector<int>& preorder, vector<int>& inorder, int l, int r) {
+        if (l > r)
+            return NULL;
+
+        TreeNode* root = new TreeNode(preorder[i]);
+        i++;
+
+        int k = l;
+        while (inorder[k] != root->val)
+            k++;
+
+        root->left = solve(preorder, inorder, l, k - 1);
+        root->right = solve(preorder, inorder, k + 1, r);
+
+        return root;
+    }
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        return solve(preorder, inorder, 0, inorder.size() - 1);
+    }
+};
