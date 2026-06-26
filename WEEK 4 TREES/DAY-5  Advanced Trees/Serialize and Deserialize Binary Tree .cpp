e19d4ef3d3a4f0313fa776bba5e069b272459e2b
VIVEK KR_CODES <sid.k7985@gmail.com>
2026-06-26 12:08:22 +0530

    Add Codec class for binary tree serialization
    Implement serialization and deserialization for a binary tree.

--- WEEK 4 TREES/DAY-5  Advanced Trees/Serialize and Deserialize Binary Tree .cpp
+++ WEEK 4 TREES/DAY-5  Advanced Trees/Serialize and Deserialize Binary Tree .cpp
@@ -1,1 +1,31 @@
+class Codec {
+public:
+    string serialize(TreeNode* root) {
+        if (root == NULL)
+            return "# ";
 
+        return to_string(root->val) + " " +
+               serialize(root->left) +
+               serialize(root->right);
+    }
+
+    TreeNode* solve(stringstream &ss) {
+        string s;
+        ss >> s;
+
+        if (s == "#")
+            return NULL;
+
+        TreeNode* root = new TreeNode(stoi(s));
+
+        root->left = solve(ss);
+        root->right = solve(ss);
+
+        return root;
+    }
+
+    TreeNode* deserialize(string data) {
+        stringstream ss(data);
+        return solve(ss);
+    }
+};
