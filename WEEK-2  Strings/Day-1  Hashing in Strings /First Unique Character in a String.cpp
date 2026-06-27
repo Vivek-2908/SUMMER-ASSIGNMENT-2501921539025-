a57039bcedeb1031c5e175b9523f90cccd28713d
VIVEK KR_CODES <sid.k7985@gmail.com>
2026-06-27 11:44:31 +0530

    Implement first unique character finder in string

--- WEEK-2  Strings/Day-1  Hashing in Strings /First Unique Character in a String.cpp
+++ WEEK-2  Strings/Day-1  Hashing in Strings /First Unique Character in a String.cpp
@@ -1,1 +1,18 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int count[26] = {0};
 
+        for (char ch : s) {
+            count[ch - 'a']++;
+        }
+
+        for (int A = 0; A < s.size(); A++) {
+            if (count[s[A] - 'a'] == 1) {
+                return A;
+            }
+        }
+
+        return -1;
+    }
+};
