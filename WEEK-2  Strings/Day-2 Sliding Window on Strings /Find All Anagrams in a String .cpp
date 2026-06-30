d25f5eef17b8a8802663ca8dd3126e776b1bef46
VIVEK KR_CODES <sid.k7985@gmail.com>
2026-06-30 21:09:34 +0530

    Implement function to find all anagrams in a string

--- WEEK-2  Strings/Day-2 Sliding Window on Strings /Find All Anagrams in a String .cpp
+++ WEEK-2  Strings/Day-2 Sliding Window on Strings /Find All Anagrams in a String .cpp
@@ -1,1 +1,30 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> A;
 
+        if (s.size() < p.size())
+            return A;
+
+        vector<int> cntP(26, 0), cntS(26, 0);
+
+        for (int i = 0; i < p.size(); i++) {
+            cntP[p[i] - 'a']++;
+            cntS[s[i] - 'a']++;
+        }
+
+        if (cntP == cntS)
+            A.push_back(0);
+
+        
+        for (int i = p.size(); i < s.size(); i++) {
+            cntS[s[i] - 'a']++;                 
+            cntS[s[i - p.size()] - 'a']--;      
+
+            if (cntP == cntS)
+                A.push_back(i - p.size() + 1);
+        }
+
+        return A;
+    }
+};
