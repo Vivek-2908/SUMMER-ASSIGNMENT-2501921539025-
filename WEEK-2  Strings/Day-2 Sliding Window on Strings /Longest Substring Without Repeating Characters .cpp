95cfe1cfad90850be1d4dade68a3c3780fbd6b36
VIVEK KR_CODES <sid.k7985@gmail.com>
2026-06-30 21:18:58 +0530

    Implement lengthOfLongestSubstring function

--- WEEK-2  Strings/Day-2 Sliding Window on Strings /Longest Substring Without Repeating Characters .cpp
+++ WEEK-2  Strings/Day-2 Sliding Window on Strings /Longest Substring Without Repeating Characters .cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> st;
+        int left = 0, A = 0;
 
+        for (int right = 0; right < s.size(); right++) {
+            while (st.count(s[right])) {
+                st.erase(s[left]);
+                left++;
+            }
+
+            st.insert(s[right]);
+            A= max(A, right - left + 1);
+        }
+
+        return A;
+    }
+};
