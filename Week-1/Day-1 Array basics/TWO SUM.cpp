ce9d81ead5d026cc9c68b48383c1e7a73b85e4c3
VIVEK KR_CODES <sid.k7985@gmail.com>
2026-07-01 21:36:15 +0530

    Implement twoSum function using unordered_map

--- Week-1/Day-1 Array basics/TWO SUM.cpp
+++ Week-1/Day-1 Array basics/TWO SUM.cpp
@@ -1,1 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& A, int target) {
+        unordered_map<int, int> mp;
 
+        for (int i = 0; i < A.size(); i++) {
+            int need = target - A[i];
+
+            if (mp.count(need))
+                return {mp[need], i};
+
+            mp[A[i]] = i;
+        }
+
+        return {};
+    }
+};
