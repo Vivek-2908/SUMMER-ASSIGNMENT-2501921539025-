bd227c7e535eaba35b3b7c43fa7f823c716da80a
VIVEK KR_CODES <sid.k7985@gmail.com>
2026-07-01 21:24:00 +0530

    Implement groupAnagrams function in C++

--- WEEK-2  Strings/Day-5Mixed String Problems /Group Anagrams .cpp
+++ WEEK-2  Strings/Day-5Mixed String Problems /Group Anagrams .cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& A) {
+        unordered_map<string, vector<string>> mp;
 
+        for (string s : A) {
+            string temp = s;
+            sort(temp.begin(), temp.end());
+            mp[temp].push_back(s);
+        }
+
+        vector<vector<string>> S;
+
+        for (auto it : mp) {
+            S.push_back(it.second);
+        }
+
+        return S;
+    }
+};
