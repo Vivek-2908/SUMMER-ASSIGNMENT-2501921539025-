e32a6a7258d940b8115671dcb4415a63a7b0fc57
VIVEK KR_CODES <sid.k7985@gmail.com>
2026-07-01 21:49:11 +0530

    Implement containsDuplicate function using unordered_set

--- Week-1/Day-2 Prefix sum and sliding window/Contain Duplicate.cpp
+++ Week-1/Day-2 Prefix sum and sliding window/Contain Duplicate.cpp
@@ -1,1 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& A) {
+        unordered_set<int> st;
 
+        for (int A : A) {
+            if (st.count(A))
+                return true;
+
+            st.insert(A);
+        }
+
+        return false;
+    }
+};
