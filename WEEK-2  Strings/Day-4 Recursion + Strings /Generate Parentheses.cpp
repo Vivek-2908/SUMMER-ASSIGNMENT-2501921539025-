b22eb7016000dad0f8772bb62f6715b72ee4f7a2
VIVEK KR_CODES <sid.k7985@gmail.com>
2026-07-01 21:17:21 +0530

    Implement generateParenthesis function using recursion

--- WEEK-2  Strings/Day-4 Recursion + Strings /Generate Parentheses.cpp
+++ WEEK-2  Strings/Day-4 Recursion + Strings /Generate Parentheses.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    vector<string> A;
 
+    void solve(string s, int open, int close, int n) {
+        if (s.size() == 2 * n) {
+            A.push_back(s);
+            return;
+        }
+
+        if (open < n)
+            solve(s + "(", open + 1, close, n);
+
+        if (close < open)
+            solve(s + ")", open, close + 1, n);
+    }
+
+    vector<string> generateParenthesis(int n) {
+        solve("", 0, 0, n);
+        return A;
+    }
+};
