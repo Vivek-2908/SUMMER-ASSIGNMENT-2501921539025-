8dcf699303592793b00d589cc4ac3e97a29af3bb
VIVEK KR_CODES <sid.k7985@gmail.com>
2026-07-01 21:27:54 +0530

    Add string compression function in Solution class
    Implemented a string compression algorithm that reduces consecutive characters to a single character followed by the count.

--- WEEK-2  Strings/Day-5Mixed String Problems /String Compression.cpp
+++ WEEK-2  Strings/Day-5Mixed String Problems /String Compression.cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int i = 0, index = 0;
 
+        while (i < chars.size()) {
+            char ch = chars[i];
+            int count = 0;
+
+            while (i < chars.size() && chars[i] == ch) {
+                count++;
+                i++;
+            }
+
+            chars[index++] = ch;
+
+            if (count > 1) {
+                string A = to_string(count);
+                for (char c : A)
+                    chars[index++] = c;
+            }
+        }
+
+        return index;
+    }
+};
