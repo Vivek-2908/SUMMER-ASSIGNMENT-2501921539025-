aecfb0fd43652570c043ecd19390b95a26b81696
VIVEK KR_CODES <sid.k7985@gmail.com>
2026-07-01 21:13:36 +0530

    Implement decodeString function using stacks

--- WEEK-2  Strings/Day-4 Recursion + Strings /Decode String .cpp
+++ WEEK-2  Strings/Day-4 Recursion + Strings /Decode String .cpp
@@ -1,1 +1,36 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<int> A;
+        stack<string> B;
+        string curr = "";
+        int num = 0;
 
+        for (char c : s) {
+            if (isdigit(c))
+                num = num * 10 + (c - '0');
+            else if (c == '[') {
+                A.push(num);
+                B.push(curr);
+                num = 0;
+                curr = "";
+            }
+            else if (c == ']') {
+                string temp = B.top();
+                B.pop();
+
+                int k = A.top();
+                A.pop();
+
+                while (k--)
+                    temp += curr;
+
+                curr = temp;
+            }
+            else
+                curr += c;
+        }
+
+        return curr;
+    }
+};
