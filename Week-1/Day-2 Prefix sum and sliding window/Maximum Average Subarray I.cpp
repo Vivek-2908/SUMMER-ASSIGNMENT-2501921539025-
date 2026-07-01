9e0e08d8946e4dd2c66d9c846c272ba9dadef848
VIVEK KR_CODES <sid.k7985@gmail.com>
2026-07-01 21:52:49 +0530

    Implement sliding window for maximum average subarray

--- Week-1/Day-2 Prefix sum and sliding window/Maximum Average Subarray I.cpp
+++ Week-1/Day-2 Prefix sum and sliding window/Maximum Average Subarray I.cpp
@@ -1,1 +1,18 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& A, int k) {
+        int sum = 0;
 
+        for (int i = 0; i < k; i++)
+            sum += A[i];
+
+        int S = sum;
+
+        for (int i = k; i < A.size(); i++) {
+            sum = sum - A[i - k] + A[i];
+            S = max(S, sum);
+        }
+
+        return (double) S/ k;
+    }
+};
