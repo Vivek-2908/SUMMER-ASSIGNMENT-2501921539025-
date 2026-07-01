77e744a5f50a6b62d9d92ea540c259c3b247a8be
VIVEK KR_CODES <sid.k7985@gmail.com>
2026-07-01 21:39:05 +0530

    Implement removeDuplicates function in Solution class

--- Week-1/Day-1 Array basics/Remove Duplicate from Sorted Array.cpp
+++ Week-1/Day-1 Array basics/Remove Duplicate from Sorted Array.cpp
@@ -1,1 +1,15 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& A) {
+        int j = 1;
 
+        for (int i = 1; i < A.size(); i++) {
+            if (A[i] != A[i - 1]) {
+                A[j] = A[i];
+                j++;
+            }
+        }
+
+        return j;
+    }
+};
