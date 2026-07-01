49fd871660c14e1ad2ed5f7a44fb413c0c75e76c
VIVEK KR_CODES <sid.k7985@gmail.com>
2026-07-01 21:09:26 +0530

    Implement function to reverse words in a string

--- WEEK-2  Strings/Day-4 Recursion + Strings /Reverse Words in a String III .cpp
+++ WEEK-2  Strings/Day-4 Recursion + Strings /Reverse Words in a String III .cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int start = 0;
 
+        for (int i = 0; i <= s.size(); i++) {
+            if (i == s.size() || s[i] == ' ') {
+                int A = start;
+                int B = i - 1;
+
+                while (A < B) {
+                    swap(s[A], s[B]);
+                    A++;
+                    B--;
+                }
+
+                start = i + 1;
+            }
+        }
+
+        return s;
+    }
+};
